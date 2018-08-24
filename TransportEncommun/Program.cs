6d89fb07a5a1a6b99e47f1b24b3b10d08c2e4131
Amandine CESAR <amandine.cesar@campus-numerique-in-the-alps.com>
2018-08-24 17:27:04 +0200

    Ajoutez des fichiers projet.

---
+++ TransportEncommun/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace TransportEncommun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create("https://data.metromobilite.fr/api/linesNear/json?x=5.727725&y=45.185656&dist=500&details=true");
+
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Display the status.
+            Console.WriteLine(response.StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            //Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+
+            Station[] Stations = Station.FromJson(responseFromServer);
+
+            //for (Station Stations = 0; Stations[] ; Stations++){
+
+            //}
+
+            //Console.WriteLine(Stations[].name);
+
+           
+            Console.ReadKey();
+
+
+
+
+            //((HttpWebRequest) request).UserAgent = ".NET Framework Example Client";
+            //Console.WriteLine(((HttpWebResponse) response).StatusDescription);  
+
+            //var request = WebRequest.Create(requestUri) as HttpWebRequest;
+            //if (request != null) { }
+        }
+
+    }
+}
---
+++ TransportEncommun/Station.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportEncommun
+{
+    class Station
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double lon { get; set; }
+        public double lat { get; set; }
+        public List<string> lines { get; set; }
+
+        public static Station[] FromJson(string json) => JsonConvert.DeserializeObject<Station[]>(json, Converter.Settings);
+
+    }
+}
